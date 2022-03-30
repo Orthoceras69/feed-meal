a0022fa8e8b0bc9686e82f877ec7eed022383a2d
Orthoceras <florian.berthier@hotmail.com>
2022-03-30 14:59:25 +0200

    fix: height settings menu

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -5,47 +5,52 @@ Widget myDrawer(BuildContext context, String title) {
   return Drawer(
       backgroundColor: Palette.light,
       child: ListView(children: [
-        DrawerHeader(
-            decoration: const BoxDecoration(color: Palette.primary),
-            child: Column(children: [
-              Text(title,
-                  style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: "FredokaOne",
-                      color: Palette.dark),
-                  semanticsLabel: title)
-            ])),
+        SizedBox(
+          height: 60,
+          child: DrawerHeader(
+              decoration: const BoxDecoration(color: Palette.primary),
+              child: Column(children: [
+                Text(title,
+                    style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "FredokaOne",
+                        color: Palette.dark),
+                    semanticsLabel: title)
+              ])),
+        ),
         Padding(
             padding: const EdgeInsets.only(left: 25.0, right: 25.0),
             child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Column(
-                    children: const [
-                      ListTile(
-                          leading: Icon(Icons.event, color: Palette.dark),
-                          title: Text("Planning",
-                              style: TextStyle(color: Palette.dark))),
-                      ListTile(
-                          leading:
-                              Icon(Icons.dinner_dining, color: Palette.dark),
-                          title: Text("Repas",
-                              style: TextStyle(color: Palette.dark))),
-                      ListTile(
-                          leading: Icon(Icons.menu_book, color: Palette.dark),
-                          title: Text("Recette",
-                              style: TextStyle(color: Palette.dark))),
-                    ],
-                  ),
-                  Column(
-                    children: const [
-                      ListTile(
-                          leading: Icon(Icons.settings, color: Palette.dark),
-                          title: Text("Réglages",
-                              style: TextStyle(color: Palette.dark))),
-                    ],
-                  ),
-                ]))
+                  Column(children: [
+                    ListTile(
+                        leading: const Icon(Icons.event, color: Palette.dark),
+                        title: const Text("Planning",
+                            style: TextStyle(color: Palette.dark)),
+                        onTap: () {}),
+                    ListTile(
+                        leading: const Icon(Icons.dinner_dining,
+                            color: Palette.dark),
+                        title: const Text("Repas",
+                            style: TextStyle(color: Palette.dark)),
+                        onTap: () {}),
+                    ListTile(
+                        leading:
+                            const Icon(Icons.menu_book, color: Palette.dark),
+                        title: const Text("Recette",
+                            style: TextStyle(color: Palette.dark)),
+                        onTap: () {}),
+                    Padding(
+                        padding: const EdgeInsets.only(top: 500),
+                        child: ListTile(
+                            leading:
+                                const Icon(Icons.settings, color: Palette.dark),
+                            title: const Text("Réglages",
+                                style: TextStyle(color: Palette.dark)),
+                            onTap: () {}))
+                  ])
+                ])),
       ]));
 }
